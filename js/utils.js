d4b3ff78e1906ef9f722f99c9097326c538b250f
Souvik Banerjee <souvikb019@gmail.com>
2023-03-24 16:34:24 +0530

    Initial

---
+++ js/utils.js
@@ -0,0 +1,28 @@
+Array.prototype.parse2D = function () {
+  const rows = []
+  for (let i = 0; i < this.length; i += 16) {
+    rows.push(this.slice(i, i + 16))
+  }
+
+  return rows
+}
+
+Array.prototype.createObjectsFrom2D = function () {
+  const objects = []
+  this.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+      if (symbol === 292 || symbol === 250) {
+        objects.push(
+          new CollisionBlock({
+            position: {
+              x: x * 64,
+              y: y * 64,
+            },
+          })
+        )
+      }
+    })
+  })
+
+  return objects
+}
